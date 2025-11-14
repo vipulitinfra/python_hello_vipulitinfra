038bce8e90db24a1080eeae66568eb8e0c5cc8d3
vipulitinfra <vipulitinfra@gmail.com>
2025-11-14 13:41:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello from vipulitinfra!"
+ @app.route('/home')
+ def home():
+    return "Hello from vipulitinfra1111111111!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
